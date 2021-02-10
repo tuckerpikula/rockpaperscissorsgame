a62669b1bbd766ff1b7eea00453faabdd4c05b5d
pikulatucker@gmail.com <pikulatucker@gmail.com>
2021-02-09 23:30:06 -0800

    overhaul

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-let wins = 0;
-let loss = 0;
-let tie = 0;
+let w = 0
+  let l = 0
+  let t = 0
 
-let npc = ['rock', 'paper', 'scissors'];
+  let npc = ['r', 'p', 's']
 
-for (let i = 0; i < 10; i++) {
-  let computerGuess = npc[Math.floor(Math.random() * npc.length)]
-  console.log(computerGuess)
 
-  let answer1 = prompt('Choose rock, paper, or scissors to play')
 
-  if (answer1 === computerGuess) {
-    tie++
-  } else if ((answer1 === 'rock' && computerGuess === 'scissors') || (answer1 === 'paper' && computerGuess === 'rock') || (answer1 === 'scissor' && computerGuess === 'paper')) {
-    wins++
-    alert('You won! Score =' + wins)
-  } else {
-    loss++
-    alert('You lost=' + loss)
+    for (let i = 0; i < 5; i++) {
+    let human = prompt('Choose r, p, or s to play!')
+      let npcGuess = npc[Math.floor(Math.random() * npc.length)]
+      // console.log(human, npcGuess)
+      if (human === npcGuess) {
+    t++
+        alert('You tie')
+      } else if (human === 'r' && npcGuess === 's' ||
+        human === 'p' && npcGuess === 'r' ||
+        human === 's' && npcGuess === 'p') {
+    w++
+        alert(`You win: NPC chose: ${npcGuess}`)
+      } else {
+    l++
+        alert(`You lose: NPC chose: ${npcGuess}`)
+      } if (i === 4) {
+    alert('Wins:' + w + 'Losses:' + l + 'Ties:' + t)
   }
-}
+    }
 
-console.log('Ties =' + tie)
-console.log('Win = ' + wins)
-console.log('Loss =' + loss)
